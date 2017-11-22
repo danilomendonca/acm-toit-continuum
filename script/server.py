94a7cc252796431799cd037c38533226cfa15ee3
danilomendonca <dfmendonca@gmail.com>
2017-11-22 11:59:55 +0100

    server installing functions from repo

--- script/server.py
+++ script/server.py
@@ -4,11 +4,13 @@ import os
 from socket import *
 from subprocess import call
 
+HELLO_MSG = 'HI'
+CLIENT_IP = '127.0.0.1'
+
 s=socket(AF_INET, SOCK_DGRAM)
 s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-
-HELLO_MSG = 'HI'
+s.bind((CLIENT_IP,12345))
 
 def heartbeat():
     print 'Domain Hearbeat Started'
@@ -29,6 +31,7 @@ def recv():
              print 'Client Discovered'
              print 'Acquisition URL: ' + data
              print 'Client IP: ' + rcvr[0]
+             CLIENT_IP = rcvr[0]
              checkAcquisition(data)
 
 def identification(server_ip):
@@ -45,7 +48,24 @@ def checkAcquisition(repo_url):
         print 'Acquisition result: ', cloneRepo(repo_url)
     else:
         print repo_url + ' already acquired, checking for updates'
-        updateRepo(repo_url, repo_name)
+        print 'Update result: ', updateRepo(repo_url, repo_name)
+    checkFunctionsInRepo(repo_name)
+
+def checkFunctionsInRepo(repo_name):
+    print 'Checking for JS functions in repo ' + repo_name
+    for actionName in os.listdir(repo_name):
+        if actionName.endswith("Function.js"):
+            # print(os.path.join(directory, filename))
+            performInstallation(repo_name, actionName, actionName)
+            continue
+        else:
+            continue
+
+def performInstallation(repo_name, actionName, actionSourceFile):
+    print 'Installing function ' + actionName + ' as ' + actionSourceFile
+    add_function_cmd = 'wsk action create ' + actionName + ' ' + actionSourceFile + ' --web yes'
+    install_cmd = "cd " + repo_name + '; ' + add_function_cmd
+    return call(install_cmd, shell=True)
 
 def cloneRepo(repo_url):
     print 'Cloning repo' + repo_url
@@ -55,19 +75,15 @@ def updateRepo(repo_url, repo_name):
     print 'Updating repo' + repo_url
     return call("cd " + repo_name + ";git pull origin master", shell=True)
 
-CLIENT_IP = '127.0.0.1'
-s.bind((CLIENT_IP,12345))
-
 hrtb_t = threading.Thread(target=heartbeat)
 hrtb_t.start()
 
 recv_t = threading.Thread(target=recv)
 recv_t.start()
 
+#comment out in production
 time.sleep(2)
-
 recv_t.do_run = False
 recv_t.join()
-
 hrtb_t.do_run = False
 hrtb_t.join()
